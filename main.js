03fbe2cdb05982fd3c7646ab1263ec7da006492d
Nahuel Alonso <leuhan.2010@gmail.com>
2023-11-17 12:56:28 -0300

    Tarea

---
+++ main.js
@@ -0,0 +1,33 @@
+function enviarFormulario() {
+    // Obtener los valores del formulario
+    const nombre = document.getElementById('nombre').value;
+    const apellido = document.getElementById('apellido').value;
+    const fechaNacimiento = document.getElementById('fechaNacimiento').value;
+
+    // Crear un objeto con los datos del formulario
+    const datos = {
+        nombre: nombre,
+        apellido: apellido,
+        fechaNacimiento: fechaNacimiento
+    };
+
+    // Realizar la solicitud HTTP usando fetch con el método POST
+    fetch('https://jsonplaceholder.typicode.com/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(datos)
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Respuesta del servidor:', data);
+        // Aquí puedes manejar la respuesta del servidor como desees
+        alert('Registro exitoso. Respuesta del servidor: ' + JSON.stringify(data));
+    })
+    .catch(error => {
+        console.error('Error al enviar la solicitud:', error);
+        // Aquí puedes manejar los errores de la solicitud
+        alert('Error al enviar la solicitud. Verifica la consola para más detalles.');
+    });
+}
